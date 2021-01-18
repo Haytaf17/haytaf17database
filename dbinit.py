2a1601a30593bfc36d09232f4933f211b0e49e28
Haytaf17 <44180104+Haytaf17@users.noreply.github.com>
2021-01-18 15:16:41 +0300

    Add files via upload

--- dbinit.py
+++ dbinit.py
@@ -4,9 +4,58 @@ import sys
 import psycopg2 as dbapi2
 
 
-INIT_STATEMENTS = [
-    "CREATE TABLE IF NOT EXISTS DUMMY (NUM INTEGER)",
-    "INSERT INTO DUMMY VALUES (42)",
+INIT_STATEMENTS = ["""
+CREATE TABLE user(
+  UserID 		SERIAL PRIMARY KEY,
+  email 		VARCHAR(255) NOT NULL UNIQUE,
+  password 		VARCHAR(255) NOT NULL,
+  isAdmin 		INTEGER NOT NULL,
+);
+"""
+,"""
+CREATE TABLE company(
+	CompanyName 		VARCHAR(100) PRIMARY KEY,
+    NumOfProducts	 	INTEGER,
+   	AverageScore 		NUMERIC(5,2)
+);
+""","""
+CREATE TABLE product(
+	ProductNo		SERIAL PRIMARY KEY,
+   	ProductName 	VARCHAR(100) NOT NULL,
+   	CompanyName		VARCHAR(100) NOT NULL,
+   	Score			NUMERIC(5,2),
+   	numberOfVotes	INTEGER,
+   	CategoryName	VARCHAR(100),
+	FOREIGN KEY (CompanyName) REFERENCES Company (CompanyName)
+		ON DELETE CASCADE    ON UPDATE CASCADE
+);
+""","""
+CREATE TABLE companyaccount(
+	CompanyAccountID 	SERIAL PRIMARY KEY,
+    CompanyName 		VARCHAR(100) NOT NULL,
+  	email 			    VARCHAR(100) NOT NULL UNIQUE,
+    password 		VARCHAR(100) NOT NULL,
+    FOREIGN KEY (CompanyName) 	REFERENCES Company (CompanyName)
+		ON DELETE CASCADE    ON UPDATE CASCADE
+);
+""","""
+CREATE TABLE evaluation (
+  EvaluationID 		SERAIAL PRIMARY KEY,
+  UserID 		    INTEGER DEFAULT,
+  ProductNo 		INTEGER ,
+  Vote 			    INTEGER ,
+  Comment 		    VARCHAR(200) ,
+  Reply 			VARCHAR(200),
+  FOREIGN KEY (UserID)	REFERENCES USER (UserID)
+    ON DELETE CASCADE,
+  FOREIGN KEY (ProductNo) REFERENCES Product (ProductNo)
+    ON DELETE CASCADE
+);""",
+
+);
+
+
+
 ]
 
 
