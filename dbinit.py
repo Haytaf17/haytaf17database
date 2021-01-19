4c16bea6f6063e9c9c51cb25e8c86c9a19e9811d
Haytaf17 <44180104+Haytaf17@users.noreply.github.com>
2021-01-19 12:46:38 +0300

    Update dbinit.py

--- dbinit.py
+++ dbinit.py
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS companyaccount(
 	CompanyAccountID 	SERIAL PRIMARY KEY,
     CompanyName 		VARCHAR(100) NOT NULL,
   	email 			    VARCHAR(100) NOT NULL UNIQUE,
-    password 		VARCHAR(100) NOT NULL,
+    password 		VARCHAR(255) NOT NULL,
     FOREIGN KEY (CompanyName) 	REFERENCES Company (CompanyName)
 		ON DELETE CASCADE    ON UPDATE CASCADE
 );
